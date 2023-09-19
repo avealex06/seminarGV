e28ca7c3451c0c34495cd7154b4ca6d01757bd29
avealex <alex.avetisyan06@gmail.com>
2023-09-19 22:09:47 +0200

    Calc code + code executable on mac

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -20,7 +20,7 @@ namespace Calculator
              * 
              * ZADANI
              * Vytvor program ktery bude fungovat jako kalkulacka. Kroky programu budou nasledujici:
-             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8.
+             * 1) Nacte vstup pro prvni cislo od uzivatele (vyuzijte metodu Console.ReadLine() - https://learn.microsoft.com/en-us/dotnet/api/system.console.readline?view=netframework-4.8
              * 2) Zkonvertuje vstup od uzivatele ze stringu do integeru - https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number.
              * 3) Nacte vstup pro druhe cislo od uzivatele a zkonvertuje ho do integeru. (zopakovani kroku 1 a 2 pro druhe cislo)
              * 4) Nacte vstup pro ciselnou operaci. Rozmysli si, jak operace nazves. Muze to byt "soucet", "rozdil" atd. nebo napr "+", "-", nebo jakkoliv jinak.
@@ -36,9 +36,35 @@ namespace Calculator
              *       - https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/iteration-statements#the-while-statement
              * 3) Umozni uzivateli zadavat i desetinna cisla, tedy prekopej kalkulacku tak, aby umela pracovat s floaty
              */
+            Console.WriteLine("first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("operation(+,-,*,/)");
+            string op = Console.ReadLine();
+            int result = 0;
 
-            //Tento komentar smaz a misto nej zacni psat svuj prdacky kod.
-
+            if (op == "+")
+            {
+                result = a + b;
+            }
+            else if (op == "-")
+            {
+                result = a - b;
+            }
+            else if (op == "*")
+            {
+                result = a * b;
+            }
+            else if (op == "/")
+            {
+                result = a / b;
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation");
+            }
+            Console.WriteLine(result);
             Console.ReadKey(); //Toto nech jako posledni radek, aby se program neukoncil ihned, ale cekal na stisk klavesy od uzivatele.
         }
     }
